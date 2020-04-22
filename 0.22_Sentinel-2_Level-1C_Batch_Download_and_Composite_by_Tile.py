5f8727bec4c3459a238924911be78466fd17fb82
Kelly Meehan <kellysmeehan@gmail.com>
2020-04-22 15:41:31 -0700

    Edited 0.22 by adding user-selected band metadata to final composite nomenclature

--- 0.22_Sentinel-2_Level-1C_Batch_Download_and_Composite_by_Tile.py
+++ 0.22_Sentinel-2_Level-1C_Batch_Download_and_Composite_by_Tile.py
@@ -4,15 +4,15 @@
 # Name:             0.22_Sentinel-2_Level-1C_Batch_Download_by_Tile.py
 # Author:           Kelly Meehan, USBR
 # Created:          20200415
-# Updated:          20200421 
+# Updated:          20200422 
 # Version:          Created using Python 3.6.8 
 
 # Requires:         ArcGIS Pro license and sentinelsat Python package
 
-# Notes:            This script is intended to be used for a Script Tool within ArcGIS Pro; it is not intended as a stand-alone script. 
+# Notes:            This script is intended to be used for a Script Tool within ArcGIS Pro; it is not intended as a stand-alone script 
 
 # Description:      This tool will allow a user to batch download and compile (post-December 6th, 2016) 
-#                   Sentinel-2 Level-1C products using three filters: 1) date range, 2) cloud cover range, and 3) tile id.
+#                   Sentinel-2 Level-1C products using three filters: 1) date range, 2) cloud cover range, and 3) tile id
  
 # Tool setup:       The script tool's properties can be set as follows (label does not matter, only the order): 
 #                       Parameters tab:    
@@ -24,7 +24,7 @@
 #                           Date_Range_End: String (Data Type) > Required (Type) > Direction (Input)
 #                           Cloud_Range_Begin: String (Data Type) > Required (Type) > Direction (Input)
 #                           Cloud_Range_End: String (Data Type) > Required (Type) > Direction (Input)
-#                           Bands_Chosen_List: String-Multiple Values (Data Type) > Required (Type) > Direction (Input) > Value List of 01 through 12 (Filter)
+#                           Bands_Chosen: String-Multiple Values (Data Type) > Required (Type) > Direction (Input) > Value List of 01 through 12 (Filter)
 
 ###############################################################################################
 ###############################################################################################
@@ -36,7 +36,7 @@
 # 2. Run query and store resultant list of products as an ordered dictionary
 # 3. Cull query results by keeping only one file per date with the smallest size
 # 4. Download products
-# 5. Unzip Sentinel-2 product 1C zipped downloaded files 
+# 5. Unzip Sentinel-2 product Level-1C zipped downloaded files 
 # 6. Composite user selected bands
 
 #----------------------------------------------------------------------------------------------
@@ -47,9 +47,6 @@
 import os, arcpy, sentinelsat, zipfile, glob, fnmatch, collections
 
 # 0.1 Assign variables to tool parameters
-
-# User creates new temporary feature class (feature set) designating full area of interest
-#aoi_polygon = arcpy.GetParameterAsText(0)
 
 # User specifies tileid to filter query results
 tiles_chosen_list = arcpy.GetParameterAsText(0)
@@ -79,23 +76,71 @@ cloud_range_begin = arcpy.GetParameterAsText(6)
 cloud_range_end = arcpy.GetParameterAsText(7)
 
 # User selects bands to be composited
-bands_chosen_list = arcpy.GetParameterAsText(8)
+bands_chosen = arcpy.GetParameterAsText(8)
 
 # Create list out of user selected band numbers
-bands_list = bands_chosen_list.split(";")
+bands_list = bands_chosen.split(';')
 
-arcpy.AddMessage('This script will composite only Sentinel bands: ' + str(bands_list))
+# 0.2 Convert user-passed argument string of bands into organized string for naming convention
 
-# 0.2 Set environment settings
+# Convert bands_chosen string of numbers into list of integers
+bands_chosen_list = list(map(int,(bands_chosen.split(';'))))
+
+# Function to convert list of numbers into a string that more elegantly expresses instances of consecutive sequences
+#   Adapted from: https://stackoverflow.com/questions/29418693/write-ranges-of-numbers-with-dashes
+
+def organize_list_of_integers(band_numbers):
+    seq = []
+    final = []
+    last = 0
+
+    for index, val in enumerate(band_numbers):
+        # Check to see if current value is either the first in list or a consecutive number from the previous
+       
+        # If element is either first element or consecutive number, add value to sequence list
+        if last + 1 == val or index == 0:
+            seq.append(val)
+            last = val
+        # If element is not consecutive number
+        else:
+            # Either add string of first-last in the case of a sequence
+            if len(seq) > 1:
+               final.append(str(seq[0]) + '-' + str(seq[len(seq)-1]))
+            # Or just add previous single value to final
+            else:
+               final.append(str(seq[0]))
+            seq = []
+            seq.append(val)
+            last = val
+        
+        # Check to see if loop is on last number in list (seq gets converted during the next index's turn, which doesn't exist for last index)
+        if index == len(band_numbers) - 1:
+            # Either add string of first-last in the case of a sequence
+            if len(seq) > 1:
+                final.append(str(seq[0]) + '-' + str(seq[-1]))
+            # Or just add single value to final
+            else:
+                final.append(str(seq[0]))
+    # Concatenate list of string elements in final list into one string using '_' in between elements
+    final_str = '_'.join(final)
+    return final_str
+
+# Call function and assign variable to resultant string (for use at end of script in naming convention of final composite rasters)
+band_nomenclature = organize_list_of_integers(bands_chosen_list)
+
+# Print list of bands to be composited by tool
+arcpy.AddMessage('This script will composite only Sentinel bands: ' + band_nomenclature)
+
+# 0.3 Set environment settings
 
 # Set workspace to output directory
 arcpy.env.workspace = output_directory
 
-# Change working directory to output directory
-os.chdir(output_directory)
-
-# Set overwrite permissions to true in case user reruns tool (and redraws aoi)
+# Set overwrite permissions to true in case user reruns tool 
 arcpy.env.overwriteOuptut = True
+
+# 0.4 Change working directory to output directory
+os.chdir(output_directory)
 
 #----------------------------------------------------------------------------------------------
 
@@ -128,19 +173,11 @@ products_df = api.to_dataframe(products)
 # Print initial query product filenames
 arcpy.AddMessage('Initial query product filenames: ' + str(list(products_df.filename)))
 
-
-
-
-
-
-
 # Sort products based on beginposition (beginning of acquisition) and then by size
-products_df_sorted = products_df.sort_values(['beginposition', 'tileid', 'size'], ascending=[True, True, False])
-print(products_df_sorted)
+products_df_sorted = products_df.sort_values(['beginposition', 'tileid', 'size'], ascending = [True, True, False])
 
 # Keep first entry (thereby selecting the smallest) in cases where there are multiple images per day (i.e. multiple tiles contain whole area of interest) 
 products_df_unduplicated = products_df_sorted.drop_duplicates(['beginposition', 'tileid'], keep = 'first')
-print(products_df_unduplicated[['filename']])
 
 # Print number of products culled 
 arcpy.AddMessage('Number of products culled: ' + str(len(products_df) - len(products_df_unduplicated.index)))
@@ -167,7 +204,6 @@ arcpy.AddMessage('Final products were either downloaded or previously existed in
 
 # Assign a variable to a list of all zip files within path directory 
 file_list = os.listdir()
-print(file_list)
 
 # Assign variable to empty list (to which .zip files to be iterated through will be added)
 zip_list = []
@@ -180,16 +216,13 @@ for h in file_list:
             print('unzipped file' + h + ' already exsits')
         else:
             zip_list.append(h)
-
-print(zip_list)    
-
+  
 # Unzip each SAFE file
 
 # For each zip file in the list
 for i in zip_list:
     # Identify the unique path
     zip_path = os.path.abspath(i)
-    print(zip_path)
     # Unzip to folder with same name as zip file within path directory
     with zipfile.ZipFile(zip_path, 'r') as zip_ref: # https://stackoverflow.com/questions/3451111/unzipping-files-in-python
         zip_ref.extractall(output_directory)
@@ -200,51 +233,37 @@ for i in zip_list:
 
 # Get tuple of subdirectories in path
 unzipped_directories_list = glob.glob('*.SAFE')
-print(unzipped_directories_list)
 
 for j in unzipped_directories_list:
     unzipped_directory_path = os.path.abspath(j)
-    print(unzipped_directory_path)
     
     granule_folder_path = os.path.join(unzipped_directory_path, 'GRANULE')
-    print(granule_folder_path)
     
     level_1C_folder_list = os.listdir(granule_folder_path)
-    print(level_1C_folder_list)
     
     for k in level_1C_folder_list:
-        print(k)
         level_1C_folder_path = os.path.join(granule_folder_path, k)
-        print(level_1C_folder_path)
         img_folder_path = os.path.join(level_1C_folder_path, 'IMG_DATA')
-        print(img_folder_path)
         raster_list = os.listdir(img_folder_path)
-        print(raster_list)
         
         all_bands_of_interest_path_list = []
         
         for r in raster_list:
-            print(r)
             raster_path = os.path.join(img_folder_path, r)
-            print(raster_path)
             
             for b in bands_list:
-                print(b)
                 match_string = '*' + str(b) + '.jp2'
-                print(match_string)
                 if fnmatch.fnmatch(r, match_string):
                     all_bands_of_interest_path_list.append(raster_path)
+        
+        # Assign variables to composited rasters using the following nomenclature:      
+        #   MMM_MSIXXX_YYYYMMDD_Txxxxx_Bx_.img 
+        #   (i.e. MissionID_ProductLevel_SensingDate_TileNumber_BandsComposited.img)
+         
+        composite_raster_name = j.split('_')[0] + '_' + j.split('_')[1] + '_' + j.split('_')[2][:8] + '_' + j.split('_')[5] + '_B' + band_nomenclature + '.img' 
 
-        print(all_bands_of_interest_path_list)
-        
-        # Assign variables to composited rasters using the following nomenclature: MMM_MSIXXX_YYYYMMDD_Txxxxx_Composit_.img (MissionID_ProductLevel_SensingDate_TileNumber_Composite.img)
-        composite_raster_name = j.split('_')[0] + '_' + j.split('_')[1] + '_' + j.split('_')[2][:8] + '_' + j.split('_')[5] + '_Composite.img' 
-        
-        print(composite_raster_name)
-        
         composite_raster = os.path.join(output_directory, composite_raster_name)
-        print(composite_raster)
-        
+     
         arcpy.CompositeBands_management(in_rasters = all_bands_of_interest_path_list, out_raster = composite_raster)
         arcpy.AddMessage('Finished compositing ' + composite_raster_name)
         
