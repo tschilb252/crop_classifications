00a9b544b57e728e5b93be395bd670f5d8733a70
Kelly Meehan <kellysmeehan@gmail.com>
2020-05-07 13:54:52 -0700

    0.20 added csv output of product metadata; minor formatting edits

--- 0.20_Sentinel-2_Level-1C_Batch_Download_by_Area_of_Interest.py
+++ 0.20_Sentinel-2_Level-1C_Batch_Download_by_Area_of_Interest.py
@@ -4,7 +4,7 @@
 # Name:             0.20_Sentinel-2_Level-1C_Batch_Download_by_Area_of_Interest.py
 # Author:           Kelly Meehan, USBR
 # Created:          20200406
-# Updated:          20200413 
+# Updated:          20200507 
 # Version:          Created using Python 3.6.8 
 
 # Requires:         ArcGIS Pro license and sentinelsat Python package download
@@ -34,7 +34,8 @@
 # 1. Authenticate credentials to Copernicul Open Access Hub 
 # 2. Run query and store resultant list of products as an ordered dictionary
 # 3. Cull query results by keeping only one file per date with the smallest size
-# 4. Download culled products to output directory
+# 4. Generate csv of downloaded product metadata
+# 5. Download culled products to output directory
 
 #----------------------------------------------------------------------------------------------
 
@@ -74,12 +75,18 @@ cloud_range_end = arcpy.GetParameterAsText(7)
 # Set workspace to output directory
 arcpy.env.workspace = output_directory
 
+# Change working directory to output directory
+os.chdir(output_directory)
+
 # Set overwrite permissions to true in case user reruns tool (and redraws aoi)
 arcpy.env.overwriteOuptut = True
 
 # 0.3 Assign variable to the default geodatabase of the map the user is running the tool from
 aprx = arcpy.mp.ArcGISProject('CURRENT')
 default_geodatabase = aprx.defaultGeodatabase
+
+# 0.4 Change working directory to output directory
+os.chdir(output_directory)
 
 #----------------------------------------------------------------------------------------------
 
@@ -96,11 +103,12 @@ arcpy.FeaturesToJSON_conversion(in_features = aoi_polygon, out_json_file = os.pa
 # Read GeoJSON file into a GeoJSON object and then convert to Well-Known Text
 footprint = sentinelsat.geojson_to_wkt(sentinelsat.read_geojson(os.path.join(output_directory, 'aoi.geojson')))
 
+arcpy.AddMessage('Searching for Sentinel-2 Level-1C products matching query')
+
 # Search SciHub for Sentinel-2, Level 1C products for which the AOI is completely inside the footprint of the image
 products = api.query(area = footprint, area_relation = 'Contains', date = (date_range_begin, date_range_end), platformname = 'Sentinel-2', producttype = 'S2MSI1C', cloudcoverpercentage = (cloud_range_begin, cloud_range_end))
 
 # Print initial number of products returned from query 
-print(len(products))
 arcpy.AddMessage('Initial number of products returned from query: ' + str(len(products)))
 
 #----------------------------------------------------------------------------------------------
@@ -109,26 +117,36 @@ arcpy.AddMessage('Initial number of products returned from query: ' + str(len(pr
 
 # Convert to Pandas DataFrame
 products_df = api.to_dataframe(products)
-print(products_df)
 
-# View pandas data frame column headers
-print(list(products_df.columns.values))
+# Print initial query product filenames
+arcpy.AddMessage('Initial query product filenames: ' + str(list(products_df.filename)))
 
 # Sort products based on beginposition (beginning of acquisition) and then by size
 products_df_sorted = products_df.sort_values(['beginposition', 'size'], ascending=[True, True])
-print(products_df_sorted)
 
 # Keep first entry (thereby selecting the smallest) in cases where there are multiple images per day (i.e. multiple tiles contain whole area of interest) 
 products_df_unduplicated = products_df_sorted.drop_duplicates(['beginposition'], keep = 'first')
-print(products_df_unduplicated[['filename']])
 
-# Print culled number of products 
-print(len(products_df_unduplicated.index))
+# Print number of products culled 
+arcpy.AddMessage('Number of products culled: ' + str(len(products_df) - len(products_df_unduplicated.index)))
+
+# Print final number of products 
 arcpy.AddMessage('Final number of products to be downloaded: ' + str(len(products_df_unduplicated.index)))
+
+# Print final product filenames
+arcpy.AddMessage('Filenames of final products to be downloaded: ' + str(list(products_df_unduplicated.filename)))
 
 #----------------------------------------------------------------------------------------------
 
-# 4. Download culled products to output directory
+# 4. Generate csv of downloaded product metadata
+
+# Write Pandas DataFrame to csv in output directory
+products_df_unduplicated.to_csv('Sentinel-2_Level-1C_Query_by_AOI_' + date_range_begin + '-' + date_range_end + '_Clouds_' + cloud_range_begin + '-' + cloud_range_end + '_Metadata.csv')
+
+arcpy.AddMessage('Generated csv with metadata from products returned by query')
+#----------------------------------------------------------------------------------------------
+
+# 5. Download culled products to output directory
 api.download_all(products = products_df_unduplicated.index, directory_path = output_directory)
 
 # Print message confirming downloads complete
