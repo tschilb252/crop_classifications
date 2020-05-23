f7c21a13c674dd01a7ecbb1c342520720248b7b2
Kelly Meehan <kellysmeehan@gmail.com>
2020-05-22 17:52:46 -0700

    add exception catching and handling of Runtime Error if feature classes are open and Cannot acquire a lock

--- 0.60_Update_Field_Values.py
+++ 0.60_Update_Field_Values.py
@@ -88,47 +88,50 @@ def check_duplicate_id():
 # 2. For each feature class, update the attribute table fields of CLASS, CROP_TYPE, and GIS_COMMENTS, and STUDY_AREA for any features with matching FIELD_ID and REGION 
 
 def update_feature():
-    # Create list of uniform attribute table field names of which the like will be searched for 
-    desired_field_names = ['FIELD_ID', 'CLASS', 'CROP_TYPE', 'STUDY_AREA', 'GIS_COMMENTS', 'REGION']
-    
-    # Iterate through all Earlier Feature Classes
-    for i in earlier_feature_classes.split(";"): 
-        # Clear selection of Feature Class
-        arcpy.SelectLayerByAttribute_management(in_layer_or_view = i, selection_type = 'CLEAR_SELECTION')
-        # Make list of fields in feature class
-        existing_fields  = [field.name for field in arcpy.ListFields(dataset = i)]
+        # Create list of uniform attribute table field names of which the like will be searched for 
+        desired_field_names = ['FIELD_ID', 'CLASS', 'CROP_TYPE', 'STUDY_AREA', 'GIS_COMMENTS', 'REGION']
         
-        # Make empty list to which will be added existing field name that have a fuzzy match with a desired field name
-        field_names_list = []
+        # Iterate through all Earlier Feature Classes
+        for i in earlier_feature_classes.split(";"): 
+            # Clear selection of Feature Class
+            arcpy.SelectLayerByAttribute_management(in_layer_or_view = i, selection_type = 'CLEAR_SELECTION')
+            # Make list of fields in feature class
+            existing_fields  = [field.name for field in arcpy.ListFields(dataset = i)]
+            
+            # Make empty list to which will be added existing field name that have a fuzzy match with a desired field name
+            field_names_list = []
+            
+            # Create a list of existing fields that have a fuzzy match with a desired field name
+            for e in existing_fields:
+                for d in desired_field_names:
+                    e_insensitive = e.casefold()
+                    d_insensitive = d.casefold()
+                    if e_insensitive[0:6] == d_insensitive[0:6]:
+                        field_names_list.append(e)
         
-        # Create a list of existing fields that have a fuzzy match with a desired field name
-        for e in existing_fields:
-            for d in desired_field_names:
-                e_insensitive = e.casefold()
-                d_insensitive = d.casefold()
-                if e_insensitive[0:6] == d_insensitive[0:6]:
-                    field_names_list.append(e)
-    
-        # Create a sorted list so that fields can always be referenced in the same order             
-        field_names_list.sort() # Order should always be (case-insensitive): class, crop_type, field_id, gis_comments, region, study_area
-    
-        with arcpy.da.UpdateCursor(in_table = i, field_names = field_names_list) as cursor:
-            for row in cursor:
-                if row[2] == int(earlier_field_id) and row[4] == region: # NOTE: conditional test will not work without int(); region used here because there's duplicated field_ids among different regions
-                    row[0] = class_value
-                    row[1] = crop_type
-                    row[5] = study_area
-                    if row[3] is not None or not str(row[1]).strip():
-                        row[3] = gis_comment
-                    else:
-                        row[3] = str(row[3]) + '; ' + gis_comment
-                    cursor.updateRow(row)
-        arcpy.AddMessage('Updated feature with FIELD_ID: ' + str(earlier_field_id) + ' within ' + str(i))                    
-
+            # Create a sorted list so that fields can always be referenced in the same order             
+            field_names_list.sort() # Order should always be (case-insensitive): class, crop_type, field_id, gis_comments, region, study_area
+        
+            with arcpy.da.UpdateCursor(in_table = i, field_names = field_names_list) as cursor:
+                for row in cursor:
+                    if row[2] == int(earlier_field_id) and row[4] == region: # NOTE: conditional test will not work without int(); region used here because there's duplicated field_ids among different regions
+                        row[0] = class_value
+                        row[1] = crop_type
+                        row[5] = study_area
+                        if row[3] is not None or not str(row[1]).strip():
+                            row[3] = gis_comment
+                        else:
+                            row[3] = str(row[3]) + '; ' + gis_comment
+                        cursor.updateRow(row)
+            arcpy.AddMessage('Updated feature with FIELD_ID: ' + str(earlier_field_id) + ' within ' + str(i))                    
 
 #----------------------------------------------
 
-if __name__ == '__main__':
+# Call functions; catch and handle a Runtime Error of Cannot acquire a lock if feature class is open
+            
+try:
     check_duplicate_id()
     update_feature()
-    
+except RuntimeError as e:
+    arcpy.AddError(e)
+    arcpy.AddMessage('Please close feature classes and re-run tool.')
