d966ef87f07f5e7b2c655f9102c33ccf9cafc7e8
Kelly Meehan <kellysmeehan@gmail.com>
2020-05-22 17:12:31 -0700

    0.60 add check for duplicate fields; add check for populated GIS_COMMENTS field before concatenating string

--- 0.60_Update_Field_Values.py
+++ 0.60_Update_Field_Values.py
@@ -5,7 +5,7 @@
 # Name:             0.60_Update_Field_Values.py
 # Author:           Kelly Meehan, USBR
 # Created:          20181214
-# Updated:          20200521 
+# Updated:          20200522 
 # Version:          Created using Python 3.6.8 
 
 # Requires:         ArcGIS Pro 
@@ -34,7 +34,8 @@
 # This script will:
 
 # 0. Set-up
-# 1. For each feature class, update the attribute table fields of CLASS, CROP_TYPE, and GIS_COMMENTS, and STUDY_AREA for any features with matching FIELD_ID and REGION 
+# 1. Test for duplicate FIELD_ID
+# 2. For each feature class, update the attribute table fields of CLASS, CROP_TYPE, and GIS_COMMENTS, and STUDY_AREA for any features with matching FIELD_ID and REGION 
 
 ###############################################################################################
 ###############################################################################################
@@ -75,40 +76,59 @@ region =  arcpy.GetParameterAsText(8)
 
 #----------------------------------------------------------------------------------------------
 
-# 1. For each feature class, update the attribute table fields of CLASS, CROP_TYPE, and GIS_COMMENTS, and STUDY_AREA for any features with matching FIELD_ID and REGION 
+# 1. Test for duplicate FIELD_ID
+        
+def check_duplicate_id():
+    for c in earlier_feature_classes.split(';'): 
+        with arcpy.da.UpdateCursor(in_table = c, field_names = ['FIELD_ID']) as cursor:
+            field_id_values = [r[0] for r in cursor]
+            duplicates = set([x for x in field_id_values if field_id_values.count(x) > 1])
+            arcpy.AddMessage('Duplicate values for ' + str(c) + ' found were: ' + str(duplicates))
 
+# 2. For each feature class, update the attribute table fields of CLASS, CROP_TYPE, and GIS_COMMENTS, and STUDY_AREA for any features with matching FIELD_ID and REGION 
 
-# Create list of uniform attribute table field names of which the like will be searched for 
-desired_field_names = ['FIELD_ID', 'CLASS', 'CROP_TYPE', 'STUDY_AREA', 'GIS_COMMENTS', 'REGION']
-
-# Iterate through all Earlier Feature Classes
-for i in earlier_feature_classes.split(";"): 
-
-    # Make list of fields in feature class
-    existing_fields  = [field.name for field in arcpy.ListFields(dataset = i)]
+def update_feature():
+    # Create list of uniform attribute table field names of which the like will be searched for 
+    desired_field_names = ['FIELD_ID', 'CLASS', 'CROP_TYPE', 'STUDY_AREA', 'GIS_COMMENTS', 'REGION']
     
-    # Make empty list of field names matching case-insensitive element from desired_field_names
-    field_names_list = []
+    # Iterate through all Earlier Feature Classes
+    for i in earlier_feature_classes.split(";"): 
+        # Clear selection of Feature Class
+        arcpy.SelectLayerByAttribute_management(in_layer_or_view = i, selection_type = 'CLEAR_SELECTION')
+        # Make list of fields in feature class
+        existing_fields  = [field.name for field in arcpy.ListFields(dataset = i)]
+        
+        # Make empty list to which will be added existing field name that have a fuzzy match with a desired field name
+        field_names_list = []
+        
+        # Create a list of existing fields that have a fuzzy match with a desired field name
+        for e in existing_fields:
+            for d in desired_field_names:
+                e_insensitive = e.casefold()
+                d_insensitive = d.casefold()
+                if e_insensitive[0:6] == d_insensitive[0:6]:
+                    field_names_list.append(e)
     
-    # For each existing field (ignoring case sensitivity), check if field name is in the DesiredNames list, and add to field_names list if so
-    for e in existing_fields:
-        for d in desired_field_names:
-            e_insensitive = e.casefold()
-            d_insensitive = d.casefold()
-            if e_insensitive[0:6] == d_insensitive[0:6]:
-                print(e_insensitive[0:6])
-                print(d_insensitive[0:6])
-                field_names_list.append(e)
+        # Create a sorted list so that fields can always be referenced in the same order             
+        field_names_list.sort() # Order should always be (case-insensitive): class, crop_type, field_id, gis_comments, region, study_area
+    
+        with arcpy.da.UpdateCursor(in_table = i, field_names = field_names_list) as cursor:
+            for row in cursor:
+                if row[2] == int(earlier_field_id) and row[4] == region: # NOTE: conditional test will not work without int(); region used here because there's duplicated field_ids among different regions
+                    row[0] = class_value
+                    row[1] = crop_type
+                    row[5] = study_area
+                    if row[3] is not None or not str(row[1]).strip():
+                        row[3] = gis_comment
+                    else:
+                        row[3] = str(row[3]) + '; ' + gis_comment
+                    cursor.updateRow(row)
+        arcpy.AddMessage('Updated feature with FIELD_ID: ' + str(earlier_field_id) + ' within ' + str(i))                    
 
-    # Create a sorted list so that fields can always be referenced in the same order             
-    field_names_list.sort() # Order should always be (case-insensitive): class, crop_type, field_id, gis_comments, region, study_area
 
-    with arcpy.da.UpdateCursor(in_table = i, field_names = field_names_list) as cursor:
-        for row in cursor:
-            if row[2] == int(earlier_field_id) and row[4] == region: # NOTE: conditional test will not work without int(); region used here because there's duplicated field_ids among different regions
-                row[0] = class_value
-                row[1] = crop_type
-                row[3] = study_area
-                row[4] = gis_comment
-                cursor.updateRow(row)
-    arcpy.AddMessage('Updated feature with FIELD_ID: ' + str(earlier_field_id) + 'within ' + str(i))                    
+#----------------------------------------------
+
+if __name__ == '__main__':
+    check_duplicate_id()
+    update_feature()
+    
