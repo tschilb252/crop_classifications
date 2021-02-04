aa933575a5d19ad615f9628650045036c208923c
Kelly Meehan <kellysmeehan@gmail.com>
2021-02-04 13:21:16 -0800

    7.10 change order of level of iterations

--- 7.10_Iterate_Pro_Classification_Trials.py
+++ 7.10_Iterate_Pro_Classification_Trials.py
@@ -23,14 +23,15 @@
 # This script will:
 
 # 0. Set-up
-# 1. 
+# 1. Create list of tuples for all combinations of OBIA attributes to iterate through (NOTE: MEAN and SD not included as no auxilary raster used) 
+
 #----------------------------------------------------------------------------------------------
 
 # 0. Set-up
 
 # 0.0 Install necessary packages
 
-import arcpy, itertools, os, re
+import arcpy, itertools, os
 from arcpy.sa import SegmentMeanShift, TrainSupportVectorMachineClassifier, ClassifyRaster
 
 #--------------------------------------------
@@ -58,10 +59,6 @@ gdb_path = arcpy.GetParameterAsText(5)
 # User selects Training Fields Shapefile
 training_fields = arcpy.GetParameterAsText(6)
 
-# User selects two digit classification iteration number
-#iteration_number = arcpy.GetParameterAsText(6)
-
-
 #--------------------------------------------
 
 # 0.2 Set environment settings
@@ -74,71 +71,88 @@ arcpy.CheckOutExtension('Spatial')
 
 #----------------------------------------------------------------------------------------------
 
-# 1. Segment Training Samples
+# 1. Create list of tuples for all combinations of OBIA attributes to iterate through (NOTE: MEAN and SD not included as no auxilary raster used) 
+
+# Create list of all attributes to include, save for color
+segment_attributes = ['COUNT', 'COMPACTNESS', 'RECTANGULARITY']
+
+# Create list of all possible combinations with three attributes
+unique_segment_attributes = []
+for l in range(1, len(segment_attributes)+1):
+    for subset in itertools.combinations(segment_attributes, l):
+        unique_segment_attributes.append(subset)
+
+# Convert list of tuples to list of strings of attributes joined by semicolons
+unique_segment_attributes_list = [';'.join(w) for w in unique_segment_attributes]
+
+# Add Color attribute to each element in list to ensure it's use in every iteration
+unique_segment_attributes_list = [s + ';COLOR' for s in obia_attributes_combinations]
+unique_segment_attributes_list.insert(0, 'COLOR')
+
+#----------------------------------------------------------------------------------------------
+
+# 2. Create list of combinations of segmentation arguments to iterate through
 
 # Delete local variable if exists 
 if 'segmented_training' in locals():
     arcpy.AddMessage('delecting segmented_training')
     del segmented_training
  
-## Segment image
-
-# Create list of all parameters to consider for unique combinations
+# Create list of spectral detail arguments to iterate through and pass to spectral detail parameter
 spectral_detail_list = ['5', '10', '15', '20']
+
+# Create list of spatial detail arguments to iterate through and pass to spatial detail parameter
 spatial_detail_list = ['5', '10', '15', '20']
+
+# Create list of minimum segment size arguments to iterate through and pass to maximum segment size parameter
 min_segment_size_list = ['10', '20', '30']
 
-# Remove
-#band_indexes_list = ['2 3 7', '3 7 9']
-
+# Create list of band selection arguments to iterate through and pass to band indexes parameter
 bands_indexes_dictionary = {'2 3 7': '237', '3 7 9': '379'}
 band_indexes_list = list(bands_indexes_dictionary.keys())
+
+# Create list of all parameters iterating through in Segment Mean Shift function
 parameters_master_list = [spectral_detail_list, spatial_detail_list, min_segment_size_list, band_indexes_list]
 
+# Create one master list of tuples, each of which is a unique combination of the four individual segmentation parameter lists unpacked
 parameters_combination_list = list(itertools.product(*parameters_master_list))
 
-len(parameters_combination_list)
+# 3. Generate function to iterate through the following layers: segmentation, classifier, and classifier attributes 
 
-#### Splat operator example
-for i in parameters_combination_list:
-    print(i)
-
-def print_test(spectral, spatial, size, bands):
-    print('spectral_detail is: ' + spectral)
-    print('spatial detail is: ' + spatial)
-    print('min segment is: ' + size) 
-    print('band_indexes is: ' + bands)
-
-for i in parameters_combination_list:
-    print_test(*i)
-
-#### END Splat example
-
-training_subset_name = os.path.basename(training_subset).split(sep = '.', maxsplit = 1)[0]
-
-region_time = training_subset_name.rsplit(sep = '_', maxsplit = 2)[0]
+fields_subset_name = os.path.basename(fields_subset).rsplit(sep = '_', maxsplit = 1)[0]
 
 def iterate_segmentation_training(raster_to_segment, spectral, spatial, size, bands):
-    segmented_training_raster = SegmentMeanShift(in_raster = raster_to_segment, spectral_detail = spectral, spatial_detail = spatial, min_segment_size = size, band_indexes = bands)
     
-    training_subset_name = os.path.basename(raster_to_segment).split(sep = '.', maxsplit = 1)[0]
-
-    region_time = training_subset_name.rsplit(sep = '_', maxsplit = 1)[0]
-
-    parameters_concatenated = str(spectral) + '_' + str(spatial) + '_' + str(size) + '_' + bands_indexes_dictionary[bands]
-     
-    output_training_raster_name = region_time + '_' + parameters_concatenated + '.tif'
-    print(output_training_raster_name)
-    output_training_raster = os.path.join(img_path, output_training_raster_name)
+    # Generate segmented raster
+    #segmented_raster = SegmentMeanShift(in_raster = raster_to_segment, spectral_detail = spectral, spatial_detail = spatial, min_segment_size = size, band_indexes = bands)
     
-    # Save output
-    segmented_training_raster.save(output_training_raster)
+    #segmented_raster_name = os.path.basename(raster_to_segment).split(sep = '.', maxsplit = 1)[0]
+
+    #region_time = training_subset_name.rsplit(sep = '_', maxsplit = 1)[0]
+
+    segment_attributes_string = str(spectral) + '_' + str(spatial) + '_' + str(size) + '_' + bands_indexes_dictionary[bands]
+    print(segment_attributes_string) 
+    output_segmented_raster_name = fields_subset_name + '_' + segment_attributes_string + '.tif'
+    print(output_segmented_raster_name)
+    output_segmented_raster = os.path.join(img_path, output_segmented_raster_name)
+    print(output_segmented_raster)
+    
+    # Save segmented raster
+    #segmented_raster.save(output_training_raster)
     
     # Polygon to raster
-    segmented_polygons_name = region_time + '_' + parameters_concatenated
-    segmented_polygons = os.path.join(gdb_path, segmented_polygons_name)
+    # = region_time + '_' + parameters_concatenated
+    #segmented_polygons = os.path.join(gdb_path, segmented_polygons_name)
     
-    arcpy.conversion.RasterToPolygon(in_raster = segmented_training_raster, out_polygon_features = segmented_polygons, simplify = 'NO_SIMPLIFY', create_multipart_features = 'SINGLE_OUTER_PART')
+    #arcpy.conversion.RasterToPolygon(in_raster = segmented_raster, out_polygon_features = segmented_polygons, simplify = 'NO_SIMPLIFY', create_multipart_features = 'SINGLE_OUTER_PART')
+    
+# Iterate through each unique segmentation combinations 
+#for s in unique_segment_attributes_list:
+    
+# Iterate through each of the three machine learning classifiers 
+
+# Iterate through each unique OBIA classifier attribute combinations
+
 
 # Create all possible combinations (based on given lists) of segmentations for Field Borders Subset Raster
 for i in parameters_combination_list:
@@ -151,30 +165,6 @@ for i in parameters_combination_list:
 #----------------------------------------------------------------------------------------------
     
 #2 Train classifier
-
-arcpy.env.workspace = img_path
-
-# Make list of fields segments
-field_segment_rasters = arcpy.ListRasters(wild_card = '*MID2_T2_2017_fields_*.tif')    
-
-# Create list of tuples for all combinations of OBIA attributes to iterate through 
-# Remove MEAN and SD as only available with auxilary raster
-#attributes_possible = ['COLOR', 'MEAN', 'STD', 'COUNT', 'COMPACTNESS', 'RECTANGULARITY']
-#attributes_possible = ['COLOR', 'COUNT', 'COMPACTNESS', 'RECTANGULARITY']
-attributes_possible = ['COUNT', 'COMPACTNESS', 'RECTANGULARITY']
-
-# Create list of all possible combinations of three attributes
-obia_attributes_master_list = []
-for l in range(1, len(attributes_possible)+1):
-    for subset in itertools.combinations(attributes_possible, l):
-        obia_attributes_master_list.append(subset)
-
-# Convert list of tuples to list of strings of attributes joined by semicolons
-obia_attributes_combinations = [';'.join(w) for w in obia_attributes_master_list]
-
-# Add Color attribute to each element in list
-obia_attributes_list = [s + ';COLOR' for s in obia_attributes_combinations]
-obia_attributes_list.insert(0, 'COLOR')
 
 
 def iterate_svm_training(segmented_raster_classifying, obia_combination):
@@ -214,6 +204,7 @@ for raster in field_segment_rasters:
 # Change baseline to region_time
 # Change filed_segment_rasters to point to direct file (without needing env.workspace) or have all one function: segmentation -> train -> classification
 # Must reorganize so that parameter combinations written first, then segments made and immediately used for classification (instead of making list of segmented rasters later), or just add 'segments' to the name to distinguish from origial raster 
-
+# Add band selection as parameter
+# Add OBIA attributes as drop down list multi-value parameter 
 
 
